2a0adc27db5208a579376a3400405258811fb8f0
zouxuhui <964030504@qq.com>
2016-03-21 20:04:51 +0800

    led_tiny4412 v1

---
+++ app/src/main/java/com/example/administrator/app_0001_tiny4412_led/MainActivity.java
@@ -0,0 +1,145 @@
+package com.example.administrator.app_0001_tiny4412_led;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
+import com.example.administrator.hardlibrary.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    private boolean ledon = false;
+    private Button button = null;
+    private CheckBox checkBoxLed1 = null;
+    private CheckBox checkBoxLed2 = null;
+    private CheckBox checkBoxLed3 = null;
+    private CheckBox checkBoxLed4 = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.BUTTON);
+
+        HardControl.ledOpen();
+
+        checkBoxLed1 = (CheckBox) findViewById(R.id.LED1);
+        checkBoxLed2 = (CheckBox) findViewById(R.id.LED2);
+        checkBoxLed3 = (CheckBox) findViewById(R.id.LED3);
+        checkBoxLed4 = (CheckBox) findViewById(R.id.LED4);
+
+        final Button button = (Button) findViewById(R.id.BUTTON);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                HardControl hardcontrol = new HardControl();
+
+                ledon = !ledon;
+                if(ledon) {
+                    button.setText("ALL OFF");
+                    checkBoxLed1.setChecked(true);
+                    checkBoxLed2.setChecked(true);
+                    checkBoxLed3.setChecked(true);
+                    checkBoxLed4.setChecked(true);
+                    for (int i = 0; i < 4; i++)
+                        HardControl.ledCtrl(i, 1);
+                }
+                else {
+                    button.setText("ALL ON");
+                    checkBoxLed1.setChecked(false);
+                    checkBoxLed2.setChecked(false);
+                    checkBoxLed3.setChecked(false);
+                    checkBoxLed4.setChecked(false);
+
+                    for (int i = 0; i < 4; i++)
+                        HardControl.ledCtrl(i, 0);
+
+                }
+            }
+        });
+    }
+
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // Check which checkbox was clicked
+        switch(view.getId()) {
+            case R.id.LED1:
+                if (checked) {
+                    // Put some meat on the sandwich
+                    Toast.makeText(getApplicationContext(), "LED1 ON", Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(0, 1);
+                }
+                else {
+                    // Remove the meat
+                    Toast.makeText(getApplicationContext(), "LED1 OFF", Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(0, 0);
+                }
+                break;
+            case R.id.LED2:
+                if (checked) {
+                    // Cheese me
+                    Toast.makeText(getApplicationContext(), "LED2 ON", Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(1, 1);
+                }
+                else {
+                    // I'm lactose intolerant
+                    Toast.makeText(getApplicationContext(), "LED2 OFF", Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(1, 0);
+                }
+                break;
+            case R.id.LED3:
+                if (checked) {
+                    // Cheese me
+                    Toast.makeText(getApplicationContext(), "LED3 ON", Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(2, 1);
+                }
+                else {
+                    // I'm lactose intolerant
+                    Toast.makeText(getApplicationContext(), "LED3 OFF", Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(2, 0);
+                }
+                break;
+            case R.id.LED4:
+                if (checked) {
+                    // Cheese me
+                    Toast.makeText(getApplicationContext(), "LED4 ON", Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(3, 1);
+                }
+                else {
+                    // I'm lactose intolerant
+                    Toast.makeText(getApplicationContext(), "LED4 OFF", Toast.LENGTH_SHORT).show();
+                    HardControl.ledCtrl(3, 0);
+                }
+                break;
+            // TODO: Veggie sandwich
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
